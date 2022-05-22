c29d82183b554785add0dc6a72259b34e06d48c5
Shreyans Pathak <pathak.shreyans@gmail.com>
2022-05-22 16:10:02 -0400

    feat: :stars: added cmd line args

--- Pix2Pix.py
+++ Pix2Pix.py
@@ -14,40 +14,44 @@ import numpy as np
 torch.backends.cudnn.benchmark = True
 
 # model definition
+
+
 class Discriminator(nn.Module):
     def __init__(self, in_channels=3) -> None:
         super().__init__()
-        self.convlayers = nn.Sequential(            
+        self.convlayers = nn.Sequential(
             nn.Conv2d(in_channels=in_channels*2, out_channels=64,
                       kernel_size=4, stride=2, padding=1, padding_mode="reflect",),
             nn.LeakyReLU(0.2),
-            
+
             nn.Conv2d(in_channels=64, out_channels=128, kernel_size=4,
                       stride=2, bias=False, padding_mode="reflect",),
-            nn.BatchNorm2d(128), 
+            nn.BatchNorm2d(128),
             nn.LeakyReLU(0.2),
-            
+
             nn.Conv2d(in_channels=128, out_channels=256, kernel_size=4,
                       stride=2, bias=False, padding_mode="reflect",),
-            nn.BatchNorm2d(256), 
+            nn.BatchNorm2d(256),
             nn.LeakyReLU(0.2),
-            
+
             nn.Conv2d(in_channels=256, out_channels=512, kernel_size=4,
                       stride=1, bias=False, padding_mode="reflect",),
-            nn.BatchNorm2d(512), 
+            nn.BatchNorm2d(512),
             nn.LeakyReLU(0.2),
-            
+
             nn.Conv2d(in_channels=512, out_channels=1, kernel_size=4,
                       stride=1, padding=1, padding_mode="reflect",),
             nn.LeakyReLU(0.2),
         )
 
-    def forward(self, x, y):
-        out = torch.cat([x, y], dim=1) 
+    def forward(self, x, y) -> None:
+        out = torch.cat([x, y], dim=1)
         out = self.convlayers(out)
         return out
 
 # generator class definition
+
+
 class Generator(nn.Module):
     def encoder(self, in_channels, out_channel, is_relu=False, need_batch_norm=True):
         x = nn.Sequential(
@@ -114,8 +118,8 @@ class Generator(nn.Module):
         )
 
     def forward(self, x):
-        layer1 = self.layer1(x)        
-        layer2 = self.layer2(layer1)        
+        layer1 = self.layer1(x)
+        layer2 = self.layer2(layer1)
         layer3 = self.layer3(layer2)
         layer4 = self.layer4(layer3)
         layer5 = self.layer5(layer4)
@@ -134,19 +138,26 @@ class Generator(nn.Module):
         return self.layer15(torch.cat([layer14, layer1], 1))
 
 # global class for constants and hyperparameters
+
+
 class config:
     DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
     TRAIN_DIR = "data/daynight/train"
     VAL_DIR = "data/daynight/val"
     LEARNING_RATE = 0.0002
     BATCH_SIZE = 16
-    NUM_WORKERS = 2        
-    LAMBDA = 100    
-    NUM_EPOCHS = 10
+    NUM_WORKERS = 2
+    LAMBDA = 100
+    NUM_EPOCHS = 50
     LOAD_MODEL = False
-    SAVE_MODEL = False
+    SAVE_MODEL = True
+    FLIP_TRAIN = False
     CHECKPOINT_DISC = "disc.pth.tar"
     CHECKPOINT_GEN = "gen.pth.tar"
+    MODEL_DEFAULT = 'maps'
+    MODEL_ANIME = 'anime'
+    MODEL_DAYNIGHT = 'daynight'
+    MODE = 'train'
 
 
 class DataTransformation:
@@ -170,6 +181,7 @@ class DataTransformation:
         ]
     )
 
+
 def save_checkpoint(model, optimizer, filename="my_checkpoint.pth.tar"):
     print("=> Saving checkpoint")
     torch.save({
@@ -190,6 +202,8 @@ def load_checkpoint(checkpoint_file, model, optimizer, lr):
 '''
   This class extends the pytorch Dataset class
 '''
+
+
 class SplitData(Dataset):
     def __init__(self, root_dir) -> None:
         self.root_dir = root_dir
@@ -205,8 +219,13 @@ class SplitData(Dataset):
         # get the image shape
         image_dim = int(image.shape[1]/2)
         # print('image shape: ', image_dim)
-        input_image = image[:, :image_dim, :]
-        target_image = image[:, image_dim:, :]
+        flip = config.FLIP_TRAIN
+        if flip:
+            target_image = image[:, :image_dim, :]
+            input_image = image[:, image_dim:, :]
+        else:
+            input_image = image[:, :image_dim, :]
+            target_image = image[:, image_dim:, :]
 
         augmentations = DataTransformation.resize(
             image=input_image, image0=target_image)
@@ -214,9 +233,11 @@ class SplitData(Dataset):
         target_image = augmentations["image0"]
 
         input_image = DataTransformation.transform(image=input_image)["image"]
-        target_image = DataTransformation.tranform_mask(image=target_image)["image"]
+        target_image = DataTransformation.tranform_mask(image=target_image)[
+            "image"]
 
         return input_image, target_image
+
 
 def train_fn(
     disc, gen, loader, opt_disc, opt_gen, l1_loss, bce, g_scaler, d_scaler,
@@ -245,7 +266,7 @@ def train_fn(
         with torch.cuda.amp.autocast():
             D_fake = disc(x, y_fake)
             G_fake_loss = bce(D_fake, torch.ones_like(D_fake))
-            L1 =  l1_loss(y_fake, y) * config.LAMBDA
+            L1 = l1_loss(y_fake, y) * config.LAMBDA
             # loss = torch.nn.HuberLoss()
             G_loss = G_fake_loss + L1
 
@@ -260,10 +281,32 @@ def train_fn(
                 D_fake=torch.sigmoid(D_fake).mean().item(),
             )
 
+
+# helper functions
+def _getTrainDirectoryPath(modelname):
+    return 'data/'+modelname+'/train' if modelname != None or modelname != '' else 'data/maps/train'
+
+
+def _getValDirectoryPath(modelname):
+    return 'data/'+modelname+'/val' if modelname != None or modelname != '' else 'data/maps/val'
+
+
+def _getDiscCheckpointPath(modelname):
+    return modelname+'_'+config.CHECKPOINT_DISC if modelname != None or modelname != '' else 'maps_'+config.CHECKPOINT_DISC
+
+
+def _getGenCheckpointPath(modelname):
+    return modelname+'_'+config.CHECKPOINT_GEN if modelname != None or modelname != '' else 'maps_'+config.CHECKPOINT_GEN
+
+
 def main(args) -> None:
     # get data from the command line arguments
-    model_name = args.modelname
-    print("Model name: ", model_name)
+    config.LOAD_MODEL = True if args.mode == True else False
+    config.FLIP_TRAIN = True if args.flip.lower() == 'true' else False
+    config.NUM_EPOCHS = int(
+        args.epochs) if args.epochs != None else config.NUM_EPOCHS
+    config.MODE = args.mode if args.mode != None else config.MODE
+
     disc = Discriminator(in_channels=3).to(config.DEVICE)
     gen = Generator(in_channels=3, features=64).to(config.DEVICE)
     opt_disc = optim.Adam(
@@ -281,7 +324,7 @@ def main(args) -> None:
             config.CHECKPOINT_DISC, disc, opt_disc, config.LEARNING_RATE,
         )
 
-    train_dataset = SplitData(root_dir=config.TRAIN_DIR)
+    train_dataset = SplitData(root_dir=_getTrainDirectoryPath(args.modelname))
     train_loader = DataLoader(
         train_dataset,
         batch_size=config.BATCH_SIZE,
@@ -290,18 +333,23 @@ def main(args) -> None:
     )
     g_scaler = torch.cuda.amp.GradScaler()
     d_scaler = torch.cuda.amp.GradScaler()
-    val_dataset = SplitData(root_dir=config.VAL_DIR)
+    val_dataset = SplitData(root_dir=_getValDirectoryPath(args.modelname))
     val_loader = DataLoader(val_dataset, batch_size=1, shuffle=False)
     val_itr = iter(val_loader)
 
-    for epoch in range(config.NUM_EPOCHS):
-        train_fn(
-            disc, gen, train_loader, opt_disc, opt_gen, L1_LOSS, BCE, g_scaler, d_scaler,
-        )
+    for epoch in range(1, config.NUM_EPOCHS+1):
+        print('Epoch: {}/{}'.format(epoch, config.NUM_EPOCHS))
+
+        if(config.MODE == 'train'):
+            train_fn(
+                disc, gen, train_loader, opt_disc, opt_gen, L1_LOSS, BCE, g_scaler, d_scaler,
+            )
 
         if config.SAVE_MODEL and epoch % 5 == 0:
-            save_checkpoint(gen, opt_gen, filename=config.CHECKPOINT_GEN)
-            save_checkpoint(disc, opt_disc, filename=config.CHECKPOINT_DISC)
+            save_checkpoint(
+                gen, opt_gen, filename=_getGenCheckpointPath(args.modelname))
+            save_checkpoint(
+                disc, opt_disc, filename=_getDiscCheckpointPath(args.modelname))
 
         x, y = next(val_itr)
         # get_test_samples(gen, x, y, epoch, folder="evaluation")
@@ -310,7 +358,7 @@ def main(args) -> None:
         gen.eval()
         with torch.no_grad():
             y_fake = gen(x)
-            y_fake = y_fake * 0.5 + 0.5  # remove normalization#
+            y_fake = y_fake * 0.5 + 0.5
             save_image(y_fake, folder + f"/y_gen_{epoch}.png")
             save_image(x * 0.5 + 0.5, folder + f"/input_{epoch}.png")
             save_image(y * 0.5 + 0.5, folder + f"/label_{epoch}.png")
@@ -320,11 +368,16 @@ def main(args) -> None:
 if __name__ == "__main__":
     # setting up the argument parser to parse the command line arguments
     argparser = argparse.ArgumentParser()
-    argparser.add_argument("--cuda", default=False, help="Enable CUDA")
-    argparser.add_argument("--invert", default=False,
+    argparser.add_argument("--flip", default=False,
                            help="learn the left side of the image")
-    argparser.add_argument("--modelname", default=False, help="which model to load")
+    argparser.add_argument(
+        "--modelname", default=config.MODEL_DEFAULT, help="which model to load")
+    argparser.add_argument("--mode", default='test',
+                           help='start in train or test mode')
+    argparser.add_argument("--epochs", default=50,
+                           help="number of epochs to train")
     args = argparser.parse_args()
     print(args)
-    # run the main function
+
+    # run the main function with all the passed command line arguments
     main(args)
