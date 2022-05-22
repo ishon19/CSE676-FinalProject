ed5217c63cb3759e1290cba9b48273f62f70a132
Naman Tejaswi <tejaswinaman1@gmail.com>
2022-05-22 18:41:09 -0400

    image gen utility

---
+++ image_processing/anime_to_sketch.py
@@ -0,0 +1,47 @@
+import cv2
+
+
+img = cv2.imread('Kakashi.jpg')
+print(img)
+#cv2.imshow('Kakshi', img)
+#cv2.waitKey(2000)
+
+print(img.shape)
+
+resized_img=cv2.resize(img, (600, 600))
+#cv2.imshow('Kakshi', resized_img)
+#cv2.waitKey(2000)
+
+rgb_2_bw=cv2.cvtColor(resized_img, cv2.COLOR_BGR2GRAY)
+#cv2.imshow('img',rgb_2_bw)
+#cv2.waitKey(2000)
+
+#invert_edge=255-rgb_2_bw
+#cv2.imshow('img',invert_edge)
+#cv2.waitKey(2000)
+
+img_invert = cv2.bitwise_not(rgb_2_bw)
+cv2.imshow('img',img_invert)
+cv2.waitKey(5000)
+
+
+gaussian_blur=cv2.GaussianBlur(img_invert, (5, 5), 0)
+cv2.imshow('blur_img',gaussian_blur)
+cv2.waitKey(5000)
+
+
+final = cv2.divide(rgb_2_bw, 255 - gaussian_blur, scale=255)
+cv2.imshow('sketch',final)
+cv2.waitKey(5000)
+
+cv2.imwrite('Kakashi_sketch.jpg', final)
+sketch=cv2.imread('Kakashi_sketch.jpg')
+
+print(final.shape)
+print(sketch.shape)
+
+combined=cv2.hconcat([resized_img,sketch])
+cv2.imshow('combined',combined)
+cv2.waitKey(5000)
+
+#print(combined.shape)
